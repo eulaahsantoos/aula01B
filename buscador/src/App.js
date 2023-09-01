e74fe9a4733489a67116ab4419bc29011cdd601d
eulaahsantoos <laizasantos616@gmail.com>
2023-09-01 13:36:44 +0000

    Save2

--- buscador/src/App.js
+++ buscador/src/App.js
@@ -1,18 +1,36 @@
 
+import {useState} from "react"
 import { FiSearch } from "react-icons/fi";
 import './styles.css';
 
 function App() {
+
+    const [input, setInputo] = useState('')
+
+    function aoClicar(){
+      alert('O que tem dento do Input' + input)
+    }
+    
+  
   return (
     <div className="container">
       <h1 className="title"> Buscador de Cep</h1>
       <div className="containerInput">
-        <input type="text" placeholder="Digite aqui..."/>
+        <input type="text" placeholder="Digite aqui..." value={input} onChange={(e) =>setInputo(e.target.value)}/>
 
-        <button className="buttonSearch">
+       
+        <button className="buttonSearch" onClick={aoClicar}>
           <FiSearch size={25} color="#FFF"/>
         </button>
       </div>
+
+      <main className="main">
+        <h2>Cep:98657898267</h2>
+        <span>Rua:Katheriane</span>
+        <span>Bairro:Katheriane</span>
+        <span>Estado:Katheriane</span>
+
+        </main>
 
     </div>
   );
